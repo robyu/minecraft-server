fc6b3f668eb06850dff99f9761f973c2330ac7ba
robyu <robert.yu@gmail.com>
2021-01-30 08:38:25 -0800

    fix script

--- supervisord-config/start-minecraft-forge.sh
+++ supervisord-config/start-minecraft-forge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker stop mc
-docker rm mc
+docker stop mc_forge
+docker rm mc_forge
 
 #
 # delete server properties so that latest Dockerfile variables take effect
 rm -f /home/ryu/minecraft-data/forge-data/server.properties
 
-docker run -v /home/ryu/minecraft-data/forge-data:/data -p 25565:25565 -e VERSION=1.15.2 --name mc minecraft_ryu
+docker run -v /home/ryu/minecraft-data/forge-data:/data -p 25565:25565 -e VERSION=1.15.2 --name mc_forge minecraft_forge_ryu
 
